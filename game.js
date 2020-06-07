347325ba2c8e7bc38d7fba5f70d5bca8a9f7e35d
Hanna <kukhaha1@uhk.cz>
2020-06-07 13:30:38 +0200

    Parts 9 - 13

--- game.js
+++ game.js
@@ -10,33 +10,37 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript??",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [];
+fetch(" \"https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple\"")
+    .then( res => {
+        console.log(res);
+        return res.json();
+})
+    .then( loadedQuestions => {
+        console.log(loadedQuestions.results);
+        loadedQuestions.results.map( loadedQuestion => {
+            const formatQuestion = {
+                question: loadedQuestion.questions
+            };
+            const answerChoices= [...loadedQuestion.incorrect_answers];
+            formatQuestion.answer = Math.floor(Math.random() * 3 ) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -50,7 +54,7 @@ startGame = () => {
 };
 
 getNewQuestion = () => {
-    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score);
         return window.location.assign("end.html");          //go to the end of the page
     }
@@ -83,7 +87,7 @@ choices.forEach(choice => {
 
         const classToApply = selectedAnswer == currentQuestion.answer ? "correct": "incorrect";
 
-        if (classToApply == 'correct') {
+        if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         };
 
@@ -102,5 +106,5 @@ incrementScore = num => {
     scoreText.innerText = score;
 }
 
-startGame();
+
 
